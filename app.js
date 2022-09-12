844a65638fb02f46ef1bf8ea7c8f7be7733ad54f
ST-SIRES <113207152+ST-SIRES@users.noreply.github.com>
2022-09-12 14:15:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 Console.log ("Rodando o sistema de gerenciamento de jogos")
 Console.log("Aplicação conectando ao banco de dados")
+console.log("Alteração 1")
+console.log("Alteração 2")
+console.log("Alteração 3")
